75d5f20f3bf03e6bc5b8184263709a6a8e01fa12
pujaa02 <164018168+pujaa02@users.noreply.github.com>
2024-07-19 19:15:24 +0530

    Update new.tsx

--- new.tsx
+++ new.tsx
@@ -24,3 +24,51 @@ const ratingsWithMenuAndRestaurant = await prisma.rating.findMany({
 });
 
 console.log(ratingsWithMenuAndRestaurant);
+
+
+const ratingsWithMenuAndRestaurant = await prisma.rating.findMany({
+  select: {
+    rating: true,
+    menu: {
+      select: {
+        id: true,
+        itemName: true,
+        price: true,
+        image: true,
+        restaurant: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+      },
+    },
+  },
+});
+
+
+
+
+ratingsWithMenuAndRestaurant.forEach(rating => {
+  console.log(`Rating: ${rating.rating}`);
+  console.log(`Menu ID: ${rating.menu.id}`);
+  console.log(`Item Name: ${rating.menu.itemName}`);
+  console.log(`Price: ${rating.menu.price}`);
+  console.log(`Image: ${rating.menu.image}`);
+  console.log(`Restaurant ID: ${rating.menu.restaurant.id}`);
+  console.log(`Restaurant Name: ${rating.menu.restaurant.name}`);
+});
+
+
+ratingsWithMenuAndRestaurant.forEach(({ rating, menu }) => {
+  const { id: menuId, itemName, price, image, restaurant } = menu;
+  const { id: restaurantId, name: restaurantName } = restaurant;
+
+  console.log(`Rating: ${rating}`);
+  console.log(`Menu ID: ${menuId}`);
+  console.log(`Item Name: ${itemName}`);
+  console.log(`Price: ${price}`);
+  console.log(`Image: ${image}`);
+  console.log(`Restaurant ID: ${restaurantId}`);
+  console.log(`Restaurant Name: ${restaurantName}`);
+});
