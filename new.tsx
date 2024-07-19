2ed35f12eb76a257f091dec81ca917b38e3b23b3
pujaa02 <164018168+pujaa02@users.noreply.github.com>
2024-07-19 19:03:39 +0530

    Update new.tsx

--- new.tsx
+++ new.tsx
@@ -1,3 +1,26 @@
 select rating.rating,rating.menu_id,menu.restaurant_id,menu.item_name,menu.price,menu.image,restaurant.name
 from food_delivery.rating join food_delivery.menu on rating.menu_id=menu.id
 join food_delivery.restaurant on restaurant.id=menu.restaurant_id;
+
+
+const ratingsWithMenuAndRestaurant = await prisma.rating.findMany({
+  select: {
+    rating: true,
+    menu: {
+      select: {
+        id: true,
+        itemName: true,
+        price: true,
+        image: true,
+        restaurant: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+      },
+    },
+  },
+});
+
+console.log(ratingsWithMenuAndRestaurant);
