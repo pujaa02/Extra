1c29398f5d830aed3612e317682fcb66b52d72b4
pujaa02 <164018168+pujaa02@users.noreply.github.com>
2024-07-20 15:08:21 +0530

    Update new.tsx

--- new.tsx
+++ new.tsx
@@ -21,3 +21,6 @@
   },
   ]
  
+select rating.rating,rating.menu_id,menu.restaurant_id,menu.item_name,menu.price,menu.image,restaurant.name
+from food_delivery.rating join food_delivery.menu on rating.menu_id=menu.id
+join food_delivery.restaurant on restaurant.id=menu.restaurant_id;
