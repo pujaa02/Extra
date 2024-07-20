0a1a6520b3f67c1531fa718fff47da96197af65f
pujaa02 <164018168+pujaa02@users.noreply.github.com>
2024-07-20 15:26:22 +0530

    Update new.tsx

--- new.tsx
+++ new.tsx
@@ -24,3 +24,7 @@
 select rating.rating,rating.menu_id,menu.restaurant_id,menu.item_name,menu.price,menu.image,restaurant.name
 from food_delivery.rating join food_delivery.menu on rating.menu_id=menu.id
 join food_delivery.restaurant on restaurant.id=menu.restaurant_id;
+
+
+select rating.menu_id,(avg(rating.rating),2)
+from food_delivery.rating group by rating.menu_id;
