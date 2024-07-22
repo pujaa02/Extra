17dcc8d4d663718385a302e84f1ed992b1081ec2
pujaa02 <164018168+pujaa02@users.noreply.github.com>
2024-07-22 13:41:01 +0530

    added

--- new.tsx
+++ new.tsx
@@ -1,45 +1,117 @@
 [
   {
-    rating: 4.2,
-    menu: {
-      id: 1,
-      item_name: 'italian pizza',
-      price: 150,
-      image: 'uploads/image-1721394051818-850304740-italian.jpg',
-      restaurant: [Object]
-    }
+    id: 2,
+    item_name: 'sandwich',
+    price: 120,
+    image: 'uploads/image-1721394082834-695463509-sandwitch.jpg',
+    restaurant: { id: 1, name: 'SpiceCraft' },
+    menu_id: 2,
+    avgrate: '3.9'
   },
   {
-    rating: 3.8,
-    menu: {
-      id: 2,
-      item_name: 'sandwich',
-      price: 120,
-      image: 'uploads/image-1721394082834-695463509-sandwitch.jpg',
-      restaurant: [Object]
-    }
+    id: 3,
+    item_name: 'french fries',
+    price: 70,
+    image: 'uploads/image-1721394094453-367136742-french_fries.jpeg',
+    restaurant: { id: 1, name: 'SpiceCraft' },
+    menu_id: 3,
+    avgrate: '3.1'
   },
-  ]
- 
-select rating.rating,rating.menu_id,menu.restaurant_id,menu.item_name,menu.price,menu.image,restaurant.name
-from food_delivery.rating join food_delivery.menu on rating.menu_id=menu.id
-join food_delivery.restaurant on restaurant.id=menu.restaurant_id;
-
-
-select rating.menu_id,(avg(rating.rating),2)
-from food_delivery.rating group by rating.menu_id;
-
-
-const averageRatings = await prisma.rating.groupBy({
-  by: ['menuId'],
-  _avg: {
-    rating: true,
+  {
+    id: 4,
+    item_name: 'manchurian',
+    price: 100,
+    image: 'uploads/image-1721394281317-314881175-manchurian.jpeg',
+    restaurant: { id: 2, name: 'Ascent: Alpine-Style Cuisine' },
+    menu_id: 4,
+    avgrate: '3.3'
   },
-});
-
-const formattedAverageRatings = averageRatings.map(rating => ({
-  menuId: rating.menuId,
-  avgRating: rating._avg.rating ? rating._avg.rating.toFixed(2) : null, // Handle null cases
-}));
-
-console.log(formattedAverageRatings);
+  {
+    id: 5,
+    item_name: 'chowmein',
+    price: 80,
+    image: 'uploads/image-1721394292716-313256665-chowmein.jpg',
+    restaurant: { id: 2, name: 'Ascent: Alpine-Style Cuisine' }
+  },
+  {
+    id: 6,
+    item_name: 'fried rice',
+    price: 120,
+    image: 'uploads/image-1721394303518-272835406-fried_rice.jpg',
+    restaurant: { id: 2, name: 'Ascent: Alpine-Style Cuisine' }
+  },
+  {
+    id: 7,
+    item_name: 'chilli paneer',
+    price: 120,
+    image: 'uploads/image-1721394324538-380055601-chilli_panner.jpeg',
+    restaurant: { id: 3, name: 'Red Stiletto Restaurant' }
+  },
+  {
+    id: 8,
+    item_name: 'garlic bread',
+    price: 256,
+    image: 'uploads/image-1721394334601-783004394-garlic_bread.jpg',
+    restaurant: { id: 3, name: 'Red Stiletto Restaurant' }
+  },
+  {
+    id: 9,
+    item_name: 'fixed punjabi thali',
+    price: 250,
+    image: 'uploads/image-1721394349179-862086351-fixed_punjabi_thali.jpeg',
+    restaurant: { id: 3, name: 'Red Stiletto Restaurant' }
+  },
+  {
+    id: 10,
+    item_name: 'veg hot & sour soup',
+    price: 80,
+    image: 'uploads/image-1721394362679-425507601-hot-and-sour-soup.jpg',
+    restaurant: { id: 4, name: 'The Winstonian' }
+  },
+  {
+    id: 11,
+    item_name: 'paneer roll',
+    price: 130,
+    image: 'uploads/image-1721394378993-162379098-panner_roll.jpg',
+    restaurant: { id: 4, name: 'The Winstonian' }
+  },
+  {
+    id: 12,
+    item_name: 'vge momos',
+    price: 70,
+    image: 'uploads/image-1721394418678-643576727-veg_momos.jpg',
+    restaurant: { id: 4, name: 'The Winstonian' }
+  },
+  {
+    id: 13,
+    item_name: 'burger',
+    price: 100,
+    image: 'uploads/image-1721394432922-307305546-burger.jpg',
+    restaurant: { id: 5, name: 'The Nouveau Table' }
+  },
+  {
+    id: 14,
+    item_name: 'vadapav',
+    price: 30,
+    image: 'uploads/image-1721394462666-92334847-vadapaw.jpeg',
+    restaurant: { id: 5, name: 'The Nouveau Table' }
+  },
+  {
+    id: 16,
+    item_name: 'vadapav',
+    price: 50,
+    image: 'uploads/image-1721628271518-627598883-vadapaww11.jpg',
+    restaurant: { id: 1, name: 'SpiceCraft' },
+    menu_id: 16,
+    avgrate: '4.5'
+  },
+  {
+    id: 1,
+    item_name: 'italian pizza',
+    price: 150,
+    image: 'uploads/image-1721630196932-699672605-italian.jpg',
+    restaurant: { id: 1, name: 'SpiceCraft' },
+    menu_id: 1,
+    avgrate: '3.9'
+  }
+]
