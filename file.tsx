dfcb2a6c37aace8ec3757199c8bcfc782b9ed039
pujaa02 <164018168+pujaa02@users.noreply.github.com>
2024-07-24 19:09:46 +0530

    Added

--- file.tsx
+++ file.tsx
@@ -1,6 +1,5 @@
 import { Action, State_restID } from "../../../Types/reducer";
 
-
 export interface State_restID {
     receiverID: number;
 }
@@ -12,105 +11,13 @@ const initialState: State_restID = {
 export default function restIDReducer(state = initialState, action: Action) {
     switch (action.type) {
         case 'ADD_RESTAURANT_ID':
-            console.log(initialState, action.payload.id, "payload");
-            return { receiverID: action.payload.id }
+            return { ...state, receiverID: action.payload.id };
         case 'REMOVE_RESTAURANT_ID':
-            return { receiverID: 0 }
+            return { ...state, receiverID: 0 };
         default:
             return state;
     }
 }
-
-
-import { Menu } from "../../../Types/menu";
-import { RegData } from "../../../Types/register";
-import { RestaurantAttributes } from "../../../Types/restaurant";
-
-export const adduser = (user: RegData) => ({
-    type: 'ADD_USER',
-    payload: { user }
-})
-
-export const removeuser = () => ({
-    type: 'REMOVE_USER',
-    payload: {}
-})
-
-export const addrest = (restaurant: RestaurantAttributes) => ({
-    type: 'ADD_RESTAURANT',
-    payload: { restaurant }
-})
-
-export const removeres = () => ({
-    type: 'REMOVE_RESTAURANT',
-    payload: {}
-})
-
-export const addrestid = (id: number) => ({
-    type: 'ADD_RESTAURANT_ID',
-    payload: { id }
-})
-
-export const removerestid = () => ({
-    type: 'REMOVE_RESTAURANT_ID'
-})
-
-export const add_menu = (item: Menu) => ({
-    type: 'ADD_MENU',
-    payload: item
-})
-
-export const remove_menu = () => ({
-    type: 'REMOVE_MENU',
-})
-
-export const addmenuid = (id: number) => ({
-    type: 'ADD_MENU_ID',
-    payload: id
-})
-
-
-export const add_to_cart = (item: Menu) => ({
-    type: 'ADD_TO_CART',
-    payload: item
-})
-
-export const new_cart = (item: Menu) => ({
-    type: 'NEW_CART',
-    payload: item
-})
-
-export const increment = (id: number) => ({
-    type: 'INCREMENT_QUANTITY',
-    payload: id
-})
-
-export const decrement = (id: number) => ({
-    type: 'DECREMENT_QUANTITY',
-    payload: id
-})
-
-
-export const remove_from_cart = (id: number) => ({
-    type: 'REMOVE_FROM_CART',
-    payload: id
-})
-
-
-export const emptycart = () => ({
-    type: 'EMPTY_CART',
-})
-
-
-export const visible = () => ({
-    type: 'VISIBLE',
-    payload: {}
-})
-
-export const unvisible = () => ({
-    type: 'UNVISIBLE',
-    payload: {}
-})
 
 
 import React, { useEffect, useState } from "react";
@@ -130,32 +37,26 @@ import { State } from "../../../Types/reducer";
 const socket = io(`${REACT_APP_BACKEND_URL}`);
 
 const AdminChat: React.FC = () => {
-    const receiverID: number = useSelector((state: State) => state.restID.receiverID);
+    const receiverID = useSelector((state: State) => state.restID.receiverID);
     const [restaurant, setRestaurant] = useState<RestaurantAverage[]>([]);
-    // const [receiverID, setReceiverID] = useState<number>();
     const [chat, setChat] = useState<ChatAttributes[]>([])
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const { register, handleSubmit, } = useForm<ChatData>();
+    const { register, handleSubmit } = useForm<ChatData>();
 
-    const fetchallchat = async () => {
+    const fetchallchat = async (id: number) => {
         await instance({
             url: "/chat/getchatdata",
             method: "GET",
         })
             .then((res) => {
-                console.log(res.data.result, "result_res");
-
-                // const result: ChatAttributes[] = (res.data.result).filter((obj: ChatAttributes) => {
-                //     return (obj.sender_id == receiverID || obj.receiver_id == receiverID)
-                // })
-                // setChat(result)
+                const result = res.data.result.filter((obj: ChatAttributes) => obj.sender_id === id || obj.receiver_id === id);
+                setChat(result);
             })
             .catch((error) => {
                 handleError(error, dispatch, navigate);
             });
     }
-    // console.log(receiverID, "id");
 
     const fetchall = async () => {
         await instance({
@@ -175,6 +76,12 @@ const AdminChat: React.FC = () => {
     }, []);
 
     useEffect(() => {
+        if (receiverID) {
+            fetchallchat(receiverID);
+        }
+    }, [receiverID]);
+
+    useEffect(() => {
         socket.on('message', (msg: string) => {
             console.log(msg, "message");
         });
@@ -183,15 +90,11 @@ const AdminChat: React.FC = () => {
         };
     }, []);
 
-    const chnageID = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        console.log(event.target.value, "event value");
-        const item: RestaurantAverage[] = restaurant.filter((obj) => {
-            return obj.restaurant_id == Number(event.target.value)
-        })
-        console.log(item[0].user_id, "item");
-        dispatch(addrestid(item[0].user_id));
-        console.log(receiverID, "receivedid");
-        fetchallchat();
+    const changeID = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const item = restaurant.find(obj => obj.restaurant_id === Number(event.target.value));
+        if (item) {
+            dispatch(addrestid(item.user_id));
+        }
     }
 
     const handleinput: SubmitHandler<ChatData> = async (data: ChatData) => {
@@ -206,16 +109,16 @@ const AdminChat: React.FC = () => {
             });
         }
     }
+
     return (
         <div>
             <div className="absolute ml-3 top-44">
                 <label htmlFor="restaurant_id" className="text-xl font-bold text-slate-600">Select Restaurant to open the Chat</label>
-                <div className=" form-group flex">
-                    <select id="restaurant_id" className="form-control"  {...register("restaurant_id", {
+                <div className="form-group flex">
+                    <select id="restaurant_id" className="form-control" {...register("restaurant_id", {
                         required: true,
-                        onChange: (e) => { chnageID(e) }
-                    })}
-                    >
+                        onChange: (e) => { changeID(e) }
+                    })}>
                         <option value="">Select Restaurant</option>
                         {restaurant.map((data: RestaurantAverage) => (
                             <option key={data.restaurant_id} value={data.restaurant_id}>{data.restaurant_name}</option>
@@ -224,8 +127,7 @@ const AdminChat: React.FC = () => {
                 </div>
             </div>
             {receiverID && <div className="chat_container">
-                <div className="upper_container">
-                </div>
+                <div className="upper_container"></div>
                 <div className="bottom_container">
                     <form onSubmit={handleSubmit(handleinput)} className="space-y-6">
                         <div className="form-group flex">
@@ -233,8 +135,7 @@ const AdminChat: React.FC = () => {
                                 type="text"
                                 placeholder="message..."
                                 id="name"
-                                {...register("message", {
-                                })}
+                                {...register("message", {})}
                                 className="ml-2 mt-2 py-2 px-2"
                             />
                             <button className="sendbtn" type="submit">Send</button>
