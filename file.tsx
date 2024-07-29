b9837dcc03c08149ad13b9867cd181d6c83a7acb
pujaa02 <164018168+pujaa02@users.noreply.github.com>
2024-07-29 15:26:22 +0530

    added

--- file.tsx
+++ file.tsx
@@ -1,4 +1,54 @@
-for (const roleKey in rolePermissions) {
+import { features, rolePermissions } from './../../resources/seedData';
+import { DataSource, Repository } from 'typeorm';
+import { PermissionEntity } from 'src/entities/permission.entity';
+import { RoleEntity } from 'src/entities/role.entity';
+import { permissions, roles } from 'src/resources/seedData';
+import { FeatureEntity } from 'src/entities/feature.entity';
+import { RolePermissionFeature } from 'src/entities/rolePermissions.entity';
+
+export async function seedData(dataSource: DataSource): Promise<void> {
+  const permissionRepository: Repository<PermissionEntity> =
+    dataSource.getRepository(PermissionEntity);
+  const roleRepository: Repository<RoleEntity> =
+    dataSource.getRepository(RoleEntity);
+  const featureRepository: Repository<FeatureEntity> =
+    dataSource.getRepository(FeatureEntity);
+  const rolePermissionsRepository: Repository<RolePermissionFeature> =
+    dataSource.getRepository(RolePermissionFeature);
+
+  for (const permission of permissions) {
+    const isExist: PermissionEntity = await permissionRepository.findOne({
+      where: {
+        action: permission,
+      },
+    });
+    if (isExist) continue;
+    await permissionRepository.save({
+      action: permission,
+    });
+  }
+
+  for (const role of roles) {
+    const isExist: RoleEntity = await roleRepository.findOne({
+      where: {
+        role_name: role,
+      },
+    });
+    if (isExist) continue;
+    await roleRepository.save({ role_name: role });
+  }
+
+  for (const feature of features) {
+    const isExist: FeatureEntity = await featureRepository.findOne({
+      where: {
+        name: feature,
+      },
+    });
+    if (isExist) continue;
+    await featureRepository.save({ name: feature });
+  }
+
+  for (const roleKey in rolePermissions) {
     const role: RoleEntity = await roleRepository.findOne({
       where: { role_name: roleKey },
     });
