9f647d4602aadadf8c8391b06aaf04ac650c5e71
pujaa02 <164018168+pujaa02@users.noreply.github.com>
2024-07-29 15:25:49 +0530

    added

--- file.tsx
+++ file.tsx
@@ -1,25 +1,39 @@
-model RolePermissionFeature {
-  id         Int        @id @default(autoincrement())
-  roleId     Int
-  permissionId Int
-  featureId  Int
-
-  role       RoleEntity       @relation(fields: [roleId], references: [id])
-  permission PermissionEntity @relation(fields: [permissionId], references: [id])
-  feature    FeatureEntity    @relation(fields: [featureId], references: [id])
-}
-
-model RoleEntity {
-  id                   Int                   @id @default(autoincrement())
-  rolePermissionFeature RolePermissionFeature[]
-}
-
-model PermissionEntity {
-  id                   Int                   @id @default(autoincrement())
-  rolePermissionFeature RolePermissionFeature[]
-}
-
-model FeatureEntity {
-  id                   Int                   @id @default(autoincrement())
-  rolePermissionFeature RolePermissionFeature[]
+for (const roleKey in rolePermissions) {
+    const role: RoleEntity = await roleRepository.findOne({
+      where: { role_name: roleKey },
+    });
+    if (!role) continue;
+    for (const featureKey in rolePermissions[
+      roleKey as keyof object
+    ] as object) {
+      const feature: FeatureEntity = await featureRepository.findOne({
+        where: { name: featureKey },
+      });
+      if (!feature) continue;
+      for (const _permission of rolePermissions[roleKey as keyof object][
+        featureKey as keyof object
+      ] as string[]) {
+        const permission: PermissionEntity = await permissionRepository.findOne(
+          {
+            where: { action: _permission },
+          },
+        );
+        if (!permission) continue;
+        const isExist: RolePermissionFeature =
+          await rolePermissionsRepository.findOne({
+            where: {
+              role,
+              feature,
+              permission,
+            },
+          });
+        if (isExist) continue;
+        await rolePermissionsRepository.save({
+          role,
+          feature,
+          permission,
+        });
+      }
+    }
+  }
 }
