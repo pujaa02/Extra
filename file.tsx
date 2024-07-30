82fc1a2df6628945d6f8064bb3b266291d9f62bf
pujaa02 <164018168+pujaa02@users.noreply.github.com>
2024-07-30 09:56:23 +0530

    added

--- file.tsx
+++ file.tsx
@@ -1,82 +1,32 @@
-import { PrismaClient } from '@prisma/client';
-import { permissions, roles, features, rolePermissions } from './../../resources/seedData';
-
-const prisma = new PrismaClient();
-
-export async function seedData(): Promise<void> {
-  for (const permission of permissions) {
-    const isExist = await prisma.permissionEntity.findUnique({
+ const roleAndPermission = await this.rolePermissionRepository.getAll({
+      include: [
+        {
+          model: Feature,
+          attributes: [],
+        },
+        {
+          model: Permission,
+          attributes: [],
+        },
+        {
+          model: Role,
+          attributes: [],
+        },
+      ],
+      attributes: [
+        'access',
+        [Sequelize.col('feature.name'), 'feature_name'],
+        [Sequelize.col('role.name'), 'role_name'],
+        [Sequelize.col('feature.id'), 'feature_id'],
+        [Sequelize.col('permission.name'), 'permission_name'],
+        [Sequelize.col('permission.id'), 'permission_id'],
+      ],
       where: {
-        action: permission,
+        role_id: user.role_id,
       },
     });
-    if (isExist) continue;
-    await prisma.permissionEntity.create({
-      data: {
-        action: permission,
-      },
-    });
-  }
+    const role = await this.roleRepository.getAll({});
 
-  for (const role of roles) {
-    const isExist = await prisma.roleEntity.findUnique({
-      where: {
-        role_name: role,
-      },
-    });
-    if (isExist) continue;
-    await prisma.roleEntity.create({
-      data: { role_name: role },
-    });
-  }
+    const permission = await this.permissionRepository.getAll({});
 
-  for (const feature of features) {
-    const isExist = await prisma.featureEntity.findUnique({
-      where: {
-        name: feature,
-      },
-    });
-    if (isExist) continue;
-    await prisma.featureEntity.create({
-      data: { name: feature },
-    });
-  }
-
-  for (const roleKey in rolePermissions) {
-    const role = await prisma.roleEntity.findUnique({
-      where: { role_name: roleKey },
-    });
-    if (!role) continue;
-
-    for (const featureKey in rolePermissions[roleKey as keyof typeof rolePermissions]) {
-      const feature = await prisma.featureEntity.findUnique({
-        where: { name: featureKey },
-      });
-      if (!feature) continue;
-
-      for (const _permission of rolePermissions[roleKey as keyof typeof rolePermissions][featureKey as keyof typeof rolePermissions[typeof roleKey]]) {
-        const permission = await prisma.permissionEntity.findUnique({
-          where: { action: _permission },
-        });
-        if (!permission) continue;
-
-        const isExist = await prisma.rolePermissionFeature.findFirst({
-          where: {
-            roleId: role.id,
-            featureId: feature.id,
-            permissionId: permission.id,
-          },
-        });
-        if (isExist) continue;
-
-        await prisma.rolePermissionFeature.create({
-          data: {
-            role: { connect: { id: role.id } },
-            feature: { connect: { id: feature.id } },
-            permission: { connect: { id: permission.id } },
-          },
-        });
-      }
-    }
-  }
-}
+    const trainerRating = await this.trainerRepository.getAllSurveyRating(req, res);
