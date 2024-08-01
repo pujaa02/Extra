b76e0b3227185d43c81672420b218851d23772a6
pujaa02 <164018168+pujaa02@users.noreply.github.com>
2024-08-01 18:53:44 +0530

    Added

--- file.tsx
+++ file.tsx
@@ -1,119 +1,8 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import io from "socket.io-client"; // Import Socket.IO client
-import instance from "../../../base-axios/useAxios";
-import toast from "react-hot-toast";
-import Header from "../../../components/Header/Header";
-import "./payment.css";
-
-const socket = io("http://localhost:3000"); // Replace with your server URL
-
-const Payment: React.FC = () => {
-    // ... existing code ...
-
-    useEffect(() => {
-        // Listen for notification events from the server
-        socket.on("notification", (notification) => {
-            toast.success(notification.message); // Display notification
-        });
-
-        // Clean up the socket connection on component unmount
-        return () => {
-            socket.disconnect();
-        };
-    }, []);
-
-    const payment = async () => {
-        if (address && phone) {
-            try {
-                const res = await instance({
-                    url: `order/addorder/${user.id}`,
-                    method: "POST",
-                    data: { cart, address, phone }
-                });
-
-                if (res.data.message === "Successfully Order placed") {
-                    navigate("/dashboard/order");
-
-                    // Emit a notification event to the server
-                    socket.emit("paymentSuccess", {
-                        userId: user.id,
-                        message: "Your order has been placed successfully!",
-                    });
-                }
-            } catch (error) {
-                handleError(error, dispatch, navigate);
-            }
-        } else {
-            toast.error("Please fill the data First!!");
-        }
-    };
-
-    // ... existing code ...
-
-    return (
-        // ... existing JSX ...
-    );
-};
-
-export default Payment;
-
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import io from "socket.io-client"; // Import Socket.IO client
-import instance from "../../../base-axios/useAxios";
-import { handleError } from "../../../utils/util";
-import { State } from "../../../Types/reducer";
-import Header from "../../../components/Header/Header";
-import "./notification.css";
-
-const socket = io("http://localhost:3000"); // Replace with your server URL
-
-const Notification: React.FC = () => {
-    const [newNotifications, setNewNotifications] = useState([]);
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const driver_id = useSelector((state: State) => state.IDs.DriverID);
-
-    useEffect(() => {
-        // Fetch initial notifications
-        const fetchnotification = async () => {
-            try {
-                const res = await instance.get(`/notification/fetchnotification/${driver_id}`);
-                setNewNotifications(res.data.data.driver_notification.filter((notif) => !notif.isRead && !notif.isDeleted));
-            } catch (error) {
-                handleError(error, dispatch, navigate);
-            }
-        };
-
-        fetchnotification();
-
-        // Listen for new notifications
-        socket.on("notification", (notification) => {
-            setNewNotifications((prevNotifications) => [notification, ...prevNotifications]);
-        });
-
-        return () => {
-            socket.disconnect();
-        };
-    }, [driver_id, dispatch, navigate]);
-
-    // ... render notifications ...
-
-    return (
-        // ... existing JSX ...
-    );
-};
-
-export default Notification;
-
-
 const io = require("socket.io")(server, {
   cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
+    origin: "http://your-client-url", // Replace with your client URL
+    methods: ["GET", "POST"],
+    credentials: true // If your application requires cookies/auth
   }
 });
 
@@ -129,3 +18,92 @@ io.on("connection", (socket) => {
   });
 });
 
+
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import io from "socket.io-client";
+import instance from "../../../base-axios/useAxios";
+import { handleError } from "../../../utils/util";
+import { State } from "../../../Types/reducer";
+import "./notification.css";
+
+const socket = io("http://localhost:3000");
+
+const Notification: React.FC = () => {
+    const [newNotifications, setNewNotifications] = useState([]);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const driver_id = useSelector((state: State) => state.IDs.DriverID);
+
+    useEffect(() => {
+        const fetchnotification = async () => {
+            try {
+                const res = await instance.get(`/notification/fetchnotification/${driver_id}`);
+                setNewNotifications(res.data.data.driver_notification.filter((notif) => !notif.isRead && !notif.isDeleted));
+            } catch (error) {
+                handleError(error, dispatch, navigate);
+            }
+        };
+
+        fetchnotification();
+
+        socket.on("notification", (notification) => {
+            setNewNotifications((prevNotifications) => [notification, ...prevNotifications]);
+        });
+
+        return () => {
+            socket.off("notification"); // Unsubscribe from notification event
+        };
+    }, [driver_id, dispatch, navigate]);
+
+    // ... existing JSX ...
+};
+
+export default Notification;
+
+
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import io from "socket.io-client";
+import instance from "../../../base-axios/useAxios";
+import toast from "react-hot-toast";
+import Header from "../../../components/Header/Header";
+import "./payment.css";
+
+const socket = io("http://localhost:3000");
+
+const Payment: React.FC = () => {
+    // ... existing state and redux setup ...
+
+    useEffect(() => {
+        // Listen for notification events from the server
+        socket.on("notification", (notification) => {
+            toast.success(notification.message);
+        });
+
+        // Clean up the socket connection on component unmount
+        return () => {
+            socket.off("notification"); // Unsubscribe from notification event
+        };
+    }, []);
+
+    const payment = async () => {
+        // ... existing payment logic ...
+
+        if (res.data.message === "Successfully Order placed") {
+            navigate("/dashboard/order");
+
+            // Emit a notification event to the server
+            socket.emit("paymentSuccess", {
+                userId: user.id,
+                message: "Your order has been placed successfully!",
+            });
+        }
+    };
+
+    // ... existing JSX ...
+};
+
+export default Payment;
